ad0c3fd32be24e935c9d51761bd607a27a5ce530
Rodrigo Gomes Silva <ra43258559805@cursos.insf>
2024-09-09 08:34:16 -0300

    :tada: first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,35 @@
 import "./app.scss";
+import { Card } from "./components/card";
 import { Header } from "./components/header";
 
 export function App() {
+  const cards = [
+    {
+      titulo: "Componentes",
+      assuntos: "- Objetos e listas de Objetos.",
+      data: "26/Agosto",
+      status: "Completo",
+    },
+    {
+      titulo: "Renderização e Efeito",
+      assuntos: "- Componentes na renderização.",
+      data: "2/Setembro",
+      status: "Em andamento",
+    },
+    {
+      titulo: "Consumindo APIs - Parte 1",
+      assuntos: "- Componentes na renderização.",
+      data: "9/Setembro",
+      entregaTrabalho: true,
+    },
+    {
+      titulo: "Prova Prática e Teórica",
+      assuntos: "- Criando um projeto React.",
+      data: "16/Setembro",
+      status: "Evento",
+    },
+  ];
+
   return (
     <div className="container-pagina">
       <Header />
@@ -9,7 +37,19 @@ export function App() {
       <section className="sessao-atividades">
         <p className="atividades-titulo">Conteúdos</p>
 
-        <div className="container-atividades"></div>
+        <div className="container-atividades">
+          {cards.map((c) => {
+            return (
+              <Card
+                titulo={c.titulo}
+                assuntos={c.assuntos}
+                data={c.data}
+                status={c.status}
+                entregaTrabalho={c.entregaTrabalho}
+              />
+            );
+          })}
+        </div>
       </section>
     </div>
   );
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,19 +1,39 @@
 import "./styles.scss";
 
-export function Card() {
+export function Card({ titulo, assuntos, data, status, entregaTrabalho }) {
+  let corPrimaria = "";
+  let corSegundaria = "";
+
+  switch (status) {
+    case "Completo":
+      corPrimaria = "#A3E5BA";
+      corSegundaria = "#EFFBE2";
+      break;
+    case "Em andamento":
+      corPrimaria = "#DCA3E5";
+      corSegundaria = "#EDE2FB";
+      break;
+    case "Evento":
+      corPrimaria = "#F6E448";
+      corSegundaria = "#FAF4C1";
+      break;
+    default:
+      corPrimaria = "#C3C3C3";
+      corSegundaria = "#F2F2F2";
+      break;
+  }
+
   return (
     <div className="card" style={{ backgroundColor: corSegundaria }}>
       <div className="card-cabecalho" style={{ backgroundColor: corPrimaria }}>
-        <h2>Componentes</h2>
+        <h2>{titulo}</h2>
       </div>
       <div className="card-conteudo">
         <h3>Assuntos</h3>
-        <pre>
-          - Objetos e Listas de Objetos
-          <br />- Componentes
-        </pre>
+        <pre>{assuntos}</pre>
       </div>
-      <p className="tag">26/agosto</p>
+      <p className="tag">{data}</p>
+      {entregaTrabalho && <p className="tag">Entrega Trab.</p>}
     </div>
   );
 }
